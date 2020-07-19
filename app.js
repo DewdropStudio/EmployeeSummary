2a08cf1b1a9beab0c5b30744199fb7ea151acde0
Rocio Avila <rocio.avila@northwestern.com>
2020-07-18 22:20:51 -0500

    Added manager questions

--- app.js
+++ app.js
@@ -13,7 +13,45 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+let teamMembers = [];
 
+// Employee Question
+const employeeQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "ENTER EMPLOYEE NAME:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "ENTER E-MAIL:"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Employee's role?",
+        choices: ["engineer", "intern"]
+    },
+    
+]
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "ENTER MANAGER NAME:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "ENTER E-MAIL:",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "ENTER OFFICE NUMBER:"
+    }
+]
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
